94c60e9f2630b9ea4a0b8752ded3de36ce521a10
Jarbas De Lima <jarbas.de.lima@hotmail.com>
2020-05-29 21:18:49 -0300

    finalizando classe Repositorio

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IBaseRepositorio<TEntity> : IDisposable where TEntity : class 
+    {
+        //Adicionar
+        void Adicionar(TEntity entity);
+        //Obter
+        TEntity ObterPorId(int id);
+        //Atualizar
+        IEnumerable<TEntity> ObterTodos();
+        //Remover 
+        void Remover(TEntity entity);
+    }
+}
---
+++ QuickBuy.Repositorio/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            var UsuarioRepositorio = new UsuarioRepositorio();
+            var Usuario = new Usuario();
+
+            UsuarioRepositorio.Adicionar(Usuario);
+        }
+    }
+}
